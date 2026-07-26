d0032fb42c7bbcc4238ffddbc146d286ee6a0463
Busakrish <busakrish7237@gmail.com>
2026-07-26 23:28:42 +0530

    show todos in UI

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const addTodoBtn = document.getElementById("addTodoBtn")
 const inputTag = document.getElementById("todoInput")
+const todoListUi = document.getElementById("todoList")
 let todoText; // THis is should be populeted when user clicks on add Button
 let todos = [];
 // If we have todos in Localstorage, we will read it 
@@ -7,12 +8,28 @@ let todosString = localStorage.getItem("todos")
 if(todosString){
     todos = JSON.parse(todosString)
 }
+
+// It shows todos in UI
+const populeteTodos = ()=>{
+    let string = ""
+    for(let todo of todos){
+        string+=`<li class="todo-item ${todo.isCompleted?"completed":""}">
+            <input type="checkbox" class="todo-checkbox" ${todo.isCompleted? "checked":""}>
+            <span class="todo-text">${todo.title}</span>
+            <button class="delete-btn">×</button>
+        </li>`
+    }
+    todoListUi.innerHTML =  string
+
+}
 addTodoBtn.addEventListener("click", ()=>{
-    inputTag.value = ""
     let todo ={
         title : inputTag.value,
         isCompleted : false
     }
     todos.push(todo)
     localStorage.setItem("todos",JSON.stringify(todos))
+    // inputTag.value = ""
+    populeteTodos()
 });
+populeteTodos()
