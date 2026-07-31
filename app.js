8f405f082ecf46091966d6d30b938c03d752a16f
Busakrish <busakrish7237@gmail.com>
2026-07-31 22:25:23 +0530

    chacked or unchecked

--- app.js
+++ app.js
@@ -5,31 +5,48 @@ let todoText; // THis is should be populeted when user clicks on add Button
 let todos = [];
 // If we have todos in Localstorage, we will read it 
 let todosString = localStorage.getItem("todos")
-if(todosString){
+if (todosString) {
     todos = JSON.parse(todosString)
 }
 
 // It shows todos in UI
-const populeteTodos = ()=>{
+const populeteTodos = () => {
     let string = ""
-    for(let todo of todos){
-        string+=`<li class="todo-item ${todo.isCompleted?"completed":""}">
-            <input type="checkbox" class="todo-checkbox" ${todo.isCompleted? "checked":""}>
-            <span class="todo-text">${todo.title}</span>
-            <button class="delete-btn">×</button>
+    for (let todo of todos) {
+        string += `<li class="todo-item ${todo.isCompleted ? "completed" : ""}">
+        <input type="checkbox" class="todo-checkbox" ${todo.isCompleted ? "checked" : ""}>
+        <span class="todo-text">${todo.title}</span>
+        <button class="delete-btn">×</button>
         </li>`
     }
-    todoListUi.innerHTML =  string
-
+    todoListUi.innerHTML = string
 }
-addTodoBtn.addEventListener("click", ()=>{
-    let todo ={
-        title : inputTag.value,
-        isCompleted : false
+
+addTodoBtn.addEventListener("click", () => {
+    let todo = {
+        title: inputTag.value,
+        isCompleted: false
     }
     todos.push(todo)
-    localStorage.setItem("todos",JSON.stringify(todos))
-    // inputTag.value = ""
+    localStorage.setItem("todos", JSON.stringify(todos))
+    inputTag.value = ""
     populeteTodos()
-});
+})
 populeteTodos()
+
+const checkboxs = document.querySelectorAll(".todo-checkbox")
+
+
+checkboxs.forEach((element) => {
+    element.addEventListener('click', (e)=>{
+        console.log(e.target.checked)
+        if(e.target.checked == true){
+            element.parentNode.classList.add("completed")
+        }
+        else{
+            element.parentNode.classList.remove("completed")
+        }
+    })
+
+});
+
